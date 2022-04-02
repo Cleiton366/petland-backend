6730d35e7c2b366ac200e3393fcccaf1d09b43b8
Cleiton Queiroz <57187549+Cleiton366@users.noreply.github.com>
2022-04-02 18:28:02 -0300

    Update check_api_key.ts

--- src/middleware/check_api_key.ts
+++ src/middleware/check_api_key.ts
@@ -1,7 +1,7 @@
 import "dotenv/config.js";
 
 export const check_api_key = (req, res, next) => {
-    if(req.headers.authorization === process.env.api_key){
+    if(req.headers.authorization === process.env.API_KEY){
         next();
     } else {
         return res.json({
