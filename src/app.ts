ffb573ab16d5819d0bf08de15422ba250ed75724
Cleiton Queiroz <57187549+Cleiton366@users.noreply.github.com>
2022-05-22 19:17:05 -0300

    Update app.ts: cors added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { router as petsRouter } from "./routes/Pets";
 import { router as userRouter } from "./routes/User";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(petsRouter);
 app.use(userRouter);
