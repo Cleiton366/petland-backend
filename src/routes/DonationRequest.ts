26b12d5bf358099095c1dfed1b1f28844e69e20c
Cleiton Queiroz <57187549+Cleiton366@users.noreply.github.com>
2022-06-18 15:52:29 -0300

    Update DonationRequest

--- src/routes/DonationRequest.ts
+++ src/routes/DonationRequest.ts
@@ -5,7 +5,7 @@ import { CheckSession } from "../middleware/CheckSession";
 const donationRequestController = new DonationRequestController();
 const router = Router();
 
-router.post("/donationrequest/new", donationRequestController.newDonationRequest);
+router.post("/donationrequest/new", CheckSession, donationRequestController.newDonationRequest);
 router.post("/donationrequest/accept" , CheckSession , donationRequestController.acceptDonationRequest);
 router.delete("/donationrequest/reject" , CheckSession, donationRequestController.rejectDonationRequest);
 router.get("/donationrequest/list" , CheckSession, donationRequestController.getUserDonationRequests);
