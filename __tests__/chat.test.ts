627aace6b406f73528fc99ed901390d0ce022fdb
Cleiton Queiroz <57187549+Cleiton366@users.noreply.github.com>
2022-06-25 18:30:47 -0300

    finished chat tests

---
+++ __tests__/chat.test.ts
@@ -0,0 +1,40 @@
+import { app } from "../src/app";
+import request from "supertest";
+
+describe ("Chats", () => {
+
+    const chatId = 'fea3024c-6122-40f3-8a49-cce9b6615c27';
+    const userId = '1545028452517592';
+
+    it("should return a chat", async () => {
+        const response = await request(app)
+        .get("/chat/getChat")
+        .set("chatId", chatId);
+        expect(response.body).toHaveProperty("chatid");
+    });
+
+    it("should return user chats", async () => {
+        const response = await request(app)
+        .get("/chat/userChats")
+        .set("userId", userId);
+        expect(response.body.length).toBeGreaterThan(0);
+    });
+
+    it("should create a message", async () => {
+        const response = await request(app)
+        .post("/chat/newMessage")
+        .send({
+            message: "test message",
+            chatId: "fea3024c-6122-40f3-8a49-cce9b6615c27",
+            userId: "1545028452517592"
+        })
+        expect(response.body.status).toBe("success");
+    });
+
+    it("should return messages from a chat", async () => {
+        const response = await request(app)
+        .get("/chat/getMessages")
+        .set("chatId", chatId);
+        expect(response.body.length).toBeGreaterThan(0);
+    });
+}); 
