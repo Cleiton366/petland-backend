36e453afded237b5917f0ae503e176d159dbe8d3
Cleiton Queiroz <57187549+Cleiton366@users.noreply.github.com>
2022-06-29 16:23:22 -0300

    Update pet test: update ids for testing, removed tests that was out of scope

--- __tests__/pet.test.ts
+++ __tests__/pet.test.ts
@@ -2,32 +2,16 @@ import { app } from "../src/app";
 import request from "supertest";
 
 describe("Pets", () => {
-  let petId = "";
 
-  it("should create a new pet", async () => {
-    const response = await request(app)
-      .post("/pet/new")
-      .send({
-        pet: {
-          donatorId: "1234",
-          petName: "test pet",
-          petAddress: {
-            city: "city",
-            state: "state",
-          },
-          age: 1,
-          medicalCondition: "healthy",
-          petType: "cat",
-        },
-      });
-    expect(response.body.status).toBe("success");
-    petId = response.body.petId;
+  it("should return a pet", async () => {
+    const response = await request(app).get(`/pet/2ffa3aa5-8eae-4319-8dfd-0a518d4e4edc`);
+    expect(response.body.petid).toBe("2ffa3aa5-8eae-4319-8dfd-0a518d4e4edc");
   });
 
   it("should return a list of pets by state and city", async () => {
     const response = await request(app).get("/pet/cat/list").query({
-      city: "Quixadá",
-      state: "Ceará",
+      city: "quixada",
+      state: "ceara",
     });
     expect(response.body.length).toBeGreaterThan(0);
   });
